ff6b285d0fc01a27e08ee92933a0f87df20710ea
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-08-14 17:44:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -30,7 +30,7 @@ class script(object):
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> <code>7384550263@fam</code> 
+<b>🛍 UPI ID:</b> <code>bibekkumar@fam</code> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
