bf40ad8283880de770fbe0d6d6cb3a376f25c5a4
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-08-14 18:33:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002786322741")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002686120949"))
 
 
@@ -38,9 +38,9 @@ START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d
 # Token Verification Adding Features Is Paid So If You Want Then Dm Me
 
 
-# SHORTNER_URL = os.environ.get("SHORTNER_URL", "arolinks.com")
-# SHORTNER_API = os.environ.get("SHORTNER_API", "e03de6ce8ad141281beeac664e1adbf3f011a8f9")
-# TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "120")
+ SHORTNER_URL = os.environ.get("SHORTNER_URL", "arolinks.com")
+ SHORTNER_API = os.environ.get("SHORTNER_API", "e03de6ce8ad141281beeac664e1adbf3f011a8f9")
+ TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "120")
 
 
 
